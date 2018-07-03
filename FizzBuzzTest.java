fecd2073467fdd1fbca18cf6a80b02d4e38e0b0c
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-03 23:10:03 +0100

    Dry out Junit tests with Before annotation

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -4,39 +4,35 @@ import org.junit.Before;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzbuzz;
+
+    @Before
+    public void initObjects() {
+        fizzbuzz = new FizzBuzz();
+    }
 
         @Test
         public void checksIfItsDivisibleByFifteen () {
-            FizzBuzz fizzbuzz = new FizzBuzz();
             String answer = fizzbuzz.fb(15);
             assertEquals("FIZZBUZZ", answer);
         }
 
         @Test
         public void checksIfItsDivisibleByFive () {
-            FizzBuzz fizzbuzz = new FizzBuzz();
             String answer = fizzbuzz.fb(5);
             assertEquals("BUZZ", answer);
         }
 
         @Test
         public void checksIfItsDivisibleByThree () {
-            FizzBuzz fizzbuzz = new FizzBuzz();
             String answer = fizzbuzz.fb(3);
             assertEquals("FIZZ", answer);
         }
 
         @Test
         public void checksIfANonFizzBuzzNumberIsReturned () {
-            FizzBuzz fizzbuzz = new FizzBuzz();
             String answer = fizzbuzz.fb(2);
             assertEquals("2", answer);
         }
 }
 
-//
-//    @Before
-//    public void initObjects() {
-//        FizzBuzz fizzbuzz = new FizzBuzz();
-//        System.out.println(fizzbuzz);
-//    }
